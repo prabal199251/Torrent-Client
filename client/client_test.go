38309a8a057e3573ca8adcf0d614e9462dd55668
Prabal Verma <prabal199251@gmail.com>
2024-08-05 16:12:37 +0530

    client.go  tested successfully

--- client/client_test.go
+++ client/client_test.go
@@ -153,3 +153,82 @@ func TestRead(t *testing.T) {
 
 	assert.Equal(t, msg, expected)
 }
+
+func TestSendRequest(t *testing.T) {
+	clientConn, serverConn := createClientAndServer(t)
+	client := Client{Conn: clientConn}
+	err := client.SendRequest(1, 2, 3)
+	assert.Nil(t, err)
+	expected := []byte{
+		0x00, 0x00, 0x00, 0x0d,
+		6,
+		0x00, 0x00, 0x00, 0x01,
+		0x00, 0x00, 0x00, 0x02,
+		0x00, 0x00, 0x00, 0x03,
+	}
+	buf := make([]byte, len(expected))
+	_, err = serverConn.Read(buf)
+	assert.Nil(t, err)
+	assert.Equal(t, expected, buf)
+}
+
+func TestSendInterested(t *testing.T) {
+	clientConn, serverConn := createClientAndServer(t)
+	client := Client{Conn: clientConn}
+	err := client.SendInterested()
+	assert.Nil(t, err)
+	expected := []byte{
+		0x00, 0x00, 0x00, 0x01,
+		2,
+	}
+	buf := make([]byte, len(expected))
+	_, err = serverConn.Read(buf)
+	assert.Nil(t, err)
+	assert.Equal(t, expected, buf)
+}
+
+func TestSendNotInterested(t *testing.T) {
+	clientConn, serverConn := createClientAndServer(t)
+	client := Client{Conn: clientConn}
+	err := client.SendNotInterested()
+	assert.Nil(t, err)
+	expected := []byte{
+		0x00, 0x00, 0x00, 0x01,
+		3,
+	}
+	buf := make([]byte, len(expected))
+	_, err = serverConn.Read(buf)
+	assert.Nil(t, err)
+	assert.Equal(t, expected, buf)
+}
+
+func TestSendUnchoke(t *testing.T) {
+	clientConn, serverConn := createClientAndServer(t)
+	client := Client{Conn: clientConn}
+	err := client.SendUnchoke()
+	assert.Nil(t, err)
+	expected := []byte{
+		0x00, 0x00, 0x00, 0x01,
+		1,
+	}
+	buf := make([]byte, len(expected))
+	_, err = serverConn.Read(buf)
+	assert.Nil(t, err)
+	assert.Equal(t, expected, buf)
+}
+
+func TestSendHave(t *testing.T) {
+	clientConn, serverConn := createClientAndServer(t)
+	client := Client{Conn: clientConn}
+	err := client.SendHave(1340)
+	assert.Nil(t, err)
+	expected := []byte{
+		0x00, 0x00, 0x00, 0x05,
+		4,
+		0x00, 0x00, 0x05, 0x3c,
+	}
+	buf := make([]byte, len(expected))
+	_, err = serverConn.Read(buf)
+	assert.Nil(t, err)
+	assert.Equal(t, expected, buf)
+}
